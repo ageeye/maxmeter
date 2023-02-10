4a8d11d0b8c07d1d26a8c93f02b0b789ebe2417d
Benjamin Alterauge <5332429+ageeye@users.noreply.github.com>
2023-02-10 21:49:25 +0100

    Update cron.py

--- cron.py
+++ cron.py
@@ -12,7 +12,7 @@ def task_meter():
     print(car1.post().content)
 
 print('Start cron: ', time.ctime(time.time()) )
-schedule.every().day.at('22:00').do(task_meter)
+schedule.every().day.at('21:00').do(task_meter)
 
 while 1:
     schedule.run_pending()
