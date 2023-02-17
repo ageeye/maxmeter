e39ef5be7d3a958410a3cc22a1f4060ded1c6ce7
Benjamin Alterauge <5332429+ageeye@users.noreply.github.com>
2023-02-17 14:14:07 +0100

    Replace data with env parameter

--- cron.py
+++ cron.py
@@ -7,7 +7,7 @@ import schedule
 import time
 
 def task_meter():
-    MaxMeter.position('EAGLEUK', 'BEDFORD')
+    MaxMeter.position('EAGLEUK', environ['APP_SITE'])
     MaxMeter.login(environ['APP_KEY'], environ['APP_URL'])
     for id in ['1', '2', '3', '4', '5']:
         assetnum  = environ['APP_PREFIX'] + id
@@ -19,7 +19,7 @@ def task_meter():
         print(car.post().content)
 
 print('Start cron: ', time.ctime(time.time()) )
-schedule.every().day.at('13:02').do(task_meter)
+schedule.every().day.at(environ['APP_TIME']).do(task_meter)
 
 while 1:
     schedule.run_pending()
