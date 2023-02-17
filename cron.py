944d63b8c75b9d287ed983cdc8d1fc3e1d5c90cb
Benjamin Alterauge <5332429+ageeye@users.noreply.github.com>
2023-02-17 15:17:51 +0100

    Update cron.py

--- cron.py
+++ cron.py
@@ -7,7 +7,7 @@ import schedule
 import time
 
 def task_meter():
-    MaxMeter.position('EAGLEUK', environ['APP_SITE'])
+    MaxMeter.position('EAGLENA', environ['APP_SITE'])
     MaxMeter.login(environ['APP_KEY'], environ['APP_URL'])
     for id in ['1', '2', '3', '4', '5']:
         assetnum  = environ['APP_PREFIX'] + id
